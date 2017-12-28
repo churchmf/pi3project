582db22719a899f2780dfe1b0416563c5647e04f
Matt <churchmf@gmail.com>
2017-12-27 17:36:24 -0700

    init

---
+++ fritzwatch.py
@@ -0,0 +1,6 @@
+# Full documentation for python-picamera is available at picamera.readthedocs.org
+import picamera
+camera = picamera.PiCamera()
+#camera.capture('image.jpg')
+#camera.start_recording('video.h264')
+#camera.stop_recording()
