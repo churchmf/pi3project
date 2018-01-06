15f0e5836b7a535ad45975998deaa71ee31efc1d
Matt <churchmf@gmail.com>
2018-01-06 12:29:10 -0700

    update pathing

--- start.sh
+++ start.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
+# build angular app
+npm install -g @angular/cli
+npm --prefix ./angular-app/my-app install
+ng build ./angular-app/my-app --prod
+
+# bring up nginx and angular app
 docker-compose up
+
+# stream video to nginx
 raspivid -o - -t 0 -vf -hf -fps 10 -b 500000 | ffmpeg -re -ar 44100 -ac 2 -acodec pcm_s16le -f s16le -ac 2 -i /dev/zero -f h264 -i - -vcodec copy -acodec aac -ab 128k -g 50 -strict experimental -f flv rtmp://localhost:1935/stream/test
