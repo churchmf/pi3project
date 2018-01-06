5a5558ff0228f478976a46a57fbaa8f1cb0481ea
Matt <churchmf@gmail.com>
2018-01-06 13:09:27 -0700

    detach

--- start.sh
+++ start.sh
@@ -5,7 +5,7 @@
 #ng build ./angular-app/my-app --prod
 
 # bring up nginx and angular app
-docker-compose up
+docker-compose up -d
 
 # stream video to nginx
 raspivid -o - -t 0 -vf -hf -fps 10 -b 500000 | ffmpeg -re -ar 44100 -ac 2 -acodec pcm_s16le -f s16le -ac 2 -i /dev/zero -f h264 -i - -vcodec copy -acodec aac -ab 128k -g 50 -strict experimental -f flv rtmp://localhost:1935/stream/test
