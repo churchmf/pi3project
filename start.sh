6eddbe2ead0a95b96e10712ffe92dfd81db3e699
Matt <churchmf@gmail.com>
2018-01-07 19:28:58 -0700

    tweaks

--- start.sh
+++ start.sh
@@ -9,4 +9,4 @@ docker-compose build
 docker-compose up -d
 
 # stream video to nginx
-raspivid -o - -t 0 --mode 5 --annotate 12 | ffmpeg -loglevel warning -re -f h264 -i - -vcodec copy -f flv rtmp://localhost:1935/dash/test &
+raspivid -o - -t 0 -w 1280 -h 720 -fps 30 -b 10000000 | ffmpeg -loglevel warning -re -f h264 -i - -vcodec copy -f flv rtmp://localhost:1935/dash/test &
