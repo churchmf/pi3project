6e8c3b8c70808eda4cf612a3a73ca90742e5d6e2
Matt <churchmf@gmail.com>
2018-01-08 19:03:37 -0700

    adding dist

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 # build angular app
-npm install -g @angular/cli
-npm install --prefix ./angular-app/my-app
-ng build ./angular-app/my-app --prod
+#npm install -g @angular/cli
+#pm install --prefix ./angular-app/my-app
+#ng build ./angular-app/my-app --prod
 
-# bring up nginx and angular app
+# bring up our streaming server and angular app
 docker-compose build
 docker-compose up -d
 
-# stream video to nginx
+# stream video
 raspivid -o - -t 0 -w 1280 -h 720 -fps 30 -b 10000000 | ffmpeg -loglevel warning -re -f h264 -i - -vcodec copy -f flv rtmp://localhost:1935/dash/test &
