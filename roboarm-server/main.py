a9dd4e74af97ef4cbbb7200d8753739218275c3a
Matt <churchmf@gmail.com>
2018-02-11 15:58:18 -0700

    convert to float

--- roboarm-server/main.py
+++ roboarm-server/main.py
@@ -4,64 +4,64 @@ from flask import Flask
 app = Flask(__name__)
 arm = Arm()
 
-@app.route('/elbow/up/<any:timeout>')
+@app.route('/elbow/up/<timeout>')
 def elbow_up(timeout):
-    arm.elbow.up(timeout=timeout)
+    arm.elbow.up(timeout=float(timeout))
     return 'OK'
 
-@app.route('/elbow/down/<any:timeout>')
+@app.route('/elbow/down/<timeout>')
 def elbow_down(timeout):
-    arm.elbow.down(timeout=timeout)
+    arm.elbow.down(timeout=float(timeout))
     return 'OK'
 
-@app.route('/grips/open/<any:timeout>')
+@app.route('/grips/open/<timeout>')
 def grips_open(timeout):
-    arm.grips.open(timeout=timeout)
+    arm.grips.open(timeout=float(timeout))
     return 'OK'
 
-@app.route('/grips/close/<any:timeout>')
+@app.route('/grips/close/<timeout>')
 def grips_close(timeout):
-    arm.grips.close(timeout=timeout)
+    arm.grips.close(timeout=float(timeout))
     return 'OK'
 
-@app.route('/wrist/up/<any:timeout>')
+@app.route('/wrist/up/<timeout>')
 def wrist_up(timeout):
-    arm.wrist.up(timeout=timeout)
+    arm.wrist.up(timeout=float(timeout))
     return 'OK'
 
-@app.route('/wrist/down/<any:timeout>')
+@app.route('/wrist/down/<timeout>')
 def wrist_down(timeout):
-    arm.wrist.down(timeout=timeout)
+    arm.wrist.down(timeout=float(timeout))
     return 'OK'
 
-@app.route('/shoulder/up/<any:timeout>')
+@app.route('/shoulder/up/<timeout>')
 def shoulder_up(timeout):
-    arm.shoulder.up(timeout=timeout)
+    arm.shoulder.up(timeout=float(timeout))
     return 'OK'
 
-@app.route('/shoulder/down/<any:timeout>')
+@app.route('/shoulder/down/<timeout>')
 def shoulder_down(timeout):
-    arm.shoulder.down(timeout=timeout)
+    arm.shoulder.down(timeout=float(timeout))
     return 'OK'
 
-@app.route('/base/rotate_clock/<any:timeout>')
+@app.route('/base/rotate_clock/<timeout>')
 def rotate_clock(timeout):
-    arm.base.rotate_clock(timeout=timeout)
+    arm.base.rotate_clock(timeout=float(timeout))
     return 'OK'
 
-@app.route('/base/rotate_counter/<any:timeout>')
+@app.route('/base/rotate_counter/<timeout>')
 def rotate_counter(timeout):
-    arm.base.rotate_counter(timeout=timeout)
+    arm.base.rotate_counter(timeout=float(timeout))
     return 'OK'
 
-@app.route('/led/on/<any:timeout>')
+@app.route('/led/on/<timeout>')
 def led_on(timeout):
-    arm.led.on(timeout=timeout)
+    arm.led.on(timeout=float(timeout))
     return 'OK'
 
-@app.route('/led/off/<any:timeout>')
+@app.route('/led/off/<timeout>')
 def led_off(timeout):
-    arm.led.off(timeout=timeout)
+    arm.led.off(timeout=float(timeout))
     return 'OK'
 
 if __name__ == '__main__':
