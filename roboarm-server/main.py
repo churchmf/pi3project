97dd09b8540ee90a20c05f3095f0375fd8109384
Matt <churchmf@gmail.com>
2018-02-11 15:34:22 -0700

    clean up

--- roboarm-server/main.py
+++ roboarm-server/main.py
@@ -1,12 +1,8 @@
 from roboarm import Arm
 from flask import Flask
 
-import usb.core, usb.util, time
-
-test = usb.core.find(idVendor=0x1267, idProduct=0x001)
-
 app = Flask(__name__)
-arm = Arm(idVendor=0x1267)
+arm = Arm()
 
 @app.route('/elbow/up/<int:timeout>')
 def elbow_up(timeout):
