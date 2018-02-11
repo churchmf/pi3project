ddf7c5764ef999528e507771697b73c6ee3f491a
Matt <churchmf@gmail.com>
2018-02-11 15:55:46 -0700

    any

--- roboarm-server/main.py
+++ roboarm-server/main.py
@@ -4,62 +4,62 @@ from flask import Flask
 app = Flask(__name__)
 arm = Arm()
 
-@app.route('/elbow/up/<float:timeout>')
+@app.route('/elbow/up/<any:timeout>')
 def elbow_up(timeout):
     arm.elbow.up(timeout=timeout)
     return 'OK'
 
-@app.route('/elbow/down/<float:timeout>')
+@app.route('/elbow/down/<any:timeout>')
 def elbow_down(timeout):
     arm.elbow.down(timeout=timeout)
     return 'OK'
 
-@app.route('/grips/open/<float:timeout>')
+@app.route('/grips/open/<any:timeout>')
 def grips_open(timeout):
     arm.grips.open(timeout=timeout)
     return 'OK'
 
-@app.route('/grips/close/<float:timeout>')
+@app.route('/grips/close/<any:timeout>')
 def grips_close(timeout):
     arm.grips.close(timeout=timeout)
     return 'OK'
 
-@app.route('/wrist/up/<float:timeout>')
+@app.route('/wrist/up/<any:timeout>')
 def wrist_up(timeout):
     arm.wrist.up(timeout=timeout)
     return 'OK'
 
-@app.route('/wrist/down/<float:timeout>')
+@app.route('/wrist/down/<any:timeout>')
 def wrist_down(timeout):
     arm.wrist.down(timeout=timeout)
     return 'OK'
 
-@app.route('/shoulder/up/<float:timeout>')
+@app.route('/shoulder/up/<any:timeout>')
 def shoulder_up(timeout):
     arm.shoulder.up(timeout=timeout)
     return 'OK'
 
-@app.route('/shoulder/down/<float:timeout>')
+@app.route('/shoulder/down/<any:timeout>')
 def shoulder_down(timeout):
     arm.shoulder.down(timeout=timeout)
     return 'OK'
 
-@app.route('/base/rotate_clock/<float:timeout>')
+@app.route('/base/rotate_clock/<any:timeout>')
 def rotate_clock(timeout):
     arm.base.rotate_clock(timeout=timeout)
     return 'OK'
 
-@app.route('/base/rotate_counter/<float:timeout>')
+@app.route('/base/rotate_counter/<any:timeout>')
 def rotate_counter(timeout):
     arm.base.rotate_counter(timeout=timeout)
     return 'OK'
 
-@app.route('/led/on/<float:timeout>')
+@app.route('/led/on/<any:timeout>')
 def led_on(timeout):
     arm.led.on(timeout=timeout)
     return 'OK'
 
-@app.route('/led/off/<float:timeout>')
+@app.route('/led/off/<any:timeout>')
 def led_off(timeout):
     arm.led.off(timeout=timeout)
     return 'OK'
